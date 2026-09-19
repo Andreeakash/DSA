0695c872727d3ed72164b8d829bedb52a629014f
Andree_akash <110694207+Andreeakash@users.noreply.github.com>
2026-09-19 17:13:57 +0530

    adding new pattern and modify some code

--- Pattern/pattern5.cpp
+++ Pattern/pattern5.cpp
@@ -3,11 +3,8 @@ using namespace std;
 
 int main(){
     int Row;
-    int Col;
     cout<<"Enter the Number of RoW:";
     cin>>Row;
-    cout<<"Enter the column of Col:";
-    cin>>Col;
 
     for(int i=Row;i>0;i--){
         for(int j=0;j<i;j++){
--- Pattern/pattern6.cpp
+++ Pattern/pattern6.cpp
@@ -3,11 +3,9 @@ using namespace std;
 
 int main(){
     int Row;
-    int Col;
     cout<<"Enter the Number of RoW:";
     cin>>Row;
-    cout<<"Enter the column of Col:";
-    cin>>Col;
+
 
     for(int i=Row;i>0;i--){
         for(int j=1;j<=i;j++){
